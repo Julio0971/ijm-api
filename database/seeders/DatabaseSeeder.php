f47e4b53ff5af219dab6d2ae5038ed1945633093
Julio Adrián Morales Vásquez <dk.jmv.0971@gmail.com>
2024-11-04 10:07:29 -0600

    Se comentan seeders de subjects y answers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             QuestionSeeder::class,
-            SubjectSeeder::class,
-            AnswerSeeder::class
+            // SubjectSeeder::class,
+            // AnswerSeeder::class
         ]);
     }
 }
